040340cf4d0111a411eaf67ffa6d9923ffe83e65
willianctti <willianctti@gmail.com>
2025-01-20 19:23:30 -0300

    initial tables

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.eventostec.api.domain.address;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
